d04eaa2546696f439c5312fdcc6ba27aac24bddf
liexel <73893402+liexel@users.noreply.github.com>
2020-11-10 11:33:20 +0100

    aggiunti commenti, ordinato il codice

--- esonero_quinonez_markjoseph.c
+++ esonero_quinonez_markjoseph.c
@@ -4,82 +4,82 @@
 #include <stdlib.h>
 #include<time.h>
  int main(){
+   printf("|RC4|\n");
    //utente inserisce un testo in chiaro (plainText)
- printf("|RC4|\n");
- printf("|Inserisci plain Text:|\n");
- char plainText[128];
- fgets(plainText, 128, stdin);//fgets(plainText, sizeof(plainText), stdin); //questo comando legge la stringa scritta dall'uente
+   printf("|Inserisci plain Text:|\n");
+   char plainText[128];
+   fgets(plainText, 128, stdin);//fgets(plainText, sizeof(plainText), stdin); //questo comando legge la stringa scritta dall'uente
+   char key[128];
+   char cipherText[128];
 
- //chiave scritta dall'utente [case 1]
- char key[128];
- char cipherText[128];
+   //variabile di controllo, se l'utente preme '1' o '2' o '3'
+   int variabile;
+   //Menu dove l'utente può scegliere l'opzione desiderata
+   printf("|Scegli come vuoi criptare il messaggio|\n");
+   printf("|1|\t DIGITA UNA CHIAVE\n");
+   printf("|2|\t CHIAVE CASUALE\n");
+   printf("|3|\t ESCI\n");
+   scanf("%d",&variabile);
 
- int variabile; //variabile di controllo, se l'utente preme '1' o '2'
- //Menu dove l'utente può scegliere l'opzione desiderata
- printf("|Scegli come vuoi criptare il messaggio|\n");
- printf("|1|\t DIGITA UNA CHIAVE\n");
- printf("|2|\t CHIAVE CASUALE\n");
- printf("|3|\t ESCI\n");
- scanf("%d",&variabile);
+   switch(variabile){
+     //chiave scritta dall'utente
+      case 1 :
+      printf("|Hai scelto di scrivere la chiave!|\n");
+      //char plainText[128], cipherText[128], key[128];
+      printf("|Inserisci key:|\n");
+      scanf("%s", key);
 
-  switch(variabile){
+      printf("\n");
+      printf("|CIPHERTEXT CIFRATA E SCRITTA IN ESADECIMALE|\n");
+      //XOR
+      for(int i=0; i<strlen(plainText);i++){
+        cipherText[i]=plainText[i]^key[i];
+        printf("%x", cipherText[i]);
+      }
+      printf("\n");
 
-    case 1 : //chiave scritta dall'utente
-    printf("|Hai scelto di scrivere la chiave!|\n");
-    //char plainText[128], cipherText[128], key[128];
-    printf("|Inserisci key:|\n");
-    scanf("%s", key);
+      printf("\n");
+      printf("|PLAINTEXT DECIFRATA|\n");
+      for(int i=0; i<strlen(plainText);i++){
+        plainText[i]=cipherText[i]^key[i];
+        printf("%c", plainText[i]);
+      }
+      break;
 
-    printf("\n");
-    printf("|CIPHERTEXT CIFRATA E SCRITTA IN ESADECIMALE|\n");
-    //XOR
-    for(int i=0; i<strlen(plainText);i++){
-      cipherText[i]=plainText[i]^key[i];
-      printf("%x", cipherText[i]);
-    }
-    printf("\n");
+    //chiave scritta casuale
+      case 2 :
+      printf("\n");
+      printf("Hai scelto la modalità casuale!\n");
+      time_t t ;
+      srand((unsigned) time(&t));
+      //creomla chiave casuale lunga 128 e la stampo in esadecimale
+      printf("|CHIAVE CASUALE SCRITTA IN ESADECIMALE|\n");
+      for(int i=0; i<strlen(plainText);i++){
+        key[i]=rand()%128;
+        printf("%x", key[i]);
+      }
+      printf("\n");
+      //XOR
+      printf("\n");
+      printf("|CIPHERTEXT CIFRATA SCRITTA IN ESADECIMALE|\n");
+      for(int i=0; i<strlen(plainText);i++){
+        cipherText[i]=plainText[i]^key[i];
+        printf("%x", cipherText[i]);
+      }
+      printf("\n");
 
-    printf("\n");
-    printf("|PLAINTEXT DECIFRATA|\n");
-    for(int i=0; i<strlen(plainText);i++){
-      plainText[i]=cipherText[i]^key[i];
-      printf("%c", plainText[i]);
-    }
-    break;
+      printf("\n");
+      printf("|PLAINTEXT DECIFRATA|\n");
+      for(int i=0; i<strlen(plainText);i++){
+        plainText[i]=cipherText[i]^key[i];
+        printf("%c", plainText[i]);
+      }
+      break;
 
-    case 2 : //chiave scritta casuale
-    printf("\n");
-    printf("Hai scelto la modalità casuale!\n");
-    time_t t ;
-    srand((unsigned) time(&t));
-    //creomla chiave casuale lunga 128 e la stampo in esadecimale
-    printf("|CHIAVE CASUALE SCRITTA IN ESADECIMALE|\n");
-    for(int i=0; i<strlen(plainText);i++){
-      key[i]=rand()%128;
-      printf("%x", key[i]);
-    }
-    printf("\n");
-    //XOR
-    printf("\n");
-    printf("|CIPHERTEXT CIFRATA SCRITTA IN ESADECIMALE|\n");
-    for(int i=0; i<strlen(plainText);i++){
-      cipherText[i]=plainText[i]^key[i];
-      printf("%x", cipherText[i]);
-    }
-    printf("\n");
+      case 3: break; //annulla
 
-    printf("\n");
-    printf("|PLAINTEXT DECIFRATA|\n");
-    for(int i=0; i<strlen(plainText);i++){
-      plainText[i]=cipherText[i]^key[i];
-      printf("%c", plainText[i]);
-    }
-    break;
-
-    case 3: break; //annulla
-
-    default : //Messaggio di errore
-    printf("Opzione non riconosciuta.");
+      default : //Messaggio di errore
+      printf("Opzione non riconosciuta.");
     }
 
 }
